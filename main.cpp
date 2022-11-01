3eeee1abd97c40c0cadd493b8f484a3ee7090195
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-11-01 14:46:59 +0000

    Don't lift the whole matplotplusplus namespace in demo

--- main.cpp
+++ main.cpp
@@ -2,12 +2,10 @@
 #include <set>
 
 int main() {
-    using namespace matplot;
-
     std::set<std::vector<double>> Y = {
         {16, 5, 9, 4}, {2, 11, 7, 14}, {3, 10, 6, 15}, {13, 8, 12, 1}};
-    plot(Y);
+    matplot::plot(Y);
 
-    show();
+    matplot::show();
     return 0;
 }
