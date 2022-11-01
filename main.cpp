fb5e6a4a4f1514794fea45efabf9c82ac7a01984
Joshua Saxby <joshua.a.saxby@gmail.com>
2022-11-01 16:36:25 +0000

    Added other sample plot made by self

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
+#include <cmath>
 #include <matplot/matplot.h>
-#include <set>
 
 int main() {
-    std::set<std::vector<double>> Y = {
-        {16, 5, 9, 4}, {2, 11, 7, 14}, {3, 10, 6, 15}, {13, 8, 12, 1}};
-    matplot::plot(Y);
-
+    auto x = matplot::linspace(0, 100);
+    auto x_times_sqrt_x = matplot::transform(x, [](double x) { return sqrt(x) / x; });
+    auto x_div_sqrt_x = matplot::transform(x, [](double x) { return x / sqrt(x); });
+    auto avg = matplot::transform(x, [](double x) { return ((sqrt(x) / x) + (x / sqrt(x))) / 2; });
+    matplot::hold(matplot::on);
+    matplot::plot(x_times_sqrt_x)->display_name("√x/x");
+    matplot::plot(x_div_sqrt_x)->display_name("x/√x");
+    matplot::plot(avg)->display_name("avg");
+    matplot::legend();
     matplot::show();
     return 0;
 }
